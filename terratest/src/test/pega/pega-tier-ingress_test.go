6387f8696ed52b8fe60dba910c63aa0bb90aa349
angeloxx <angeloxx@angeloxx.it>
2024-09-03 17:30:27 +0200

    test the ingressclassname - fix test

--- terratest/src/test/pega/pega-tier-ingress_test.go
+++ terratest/src/test/pega/pega-tier-ingress_test.go
@@ -112,7 +112,7 @@ func VerifyAKSIngress(t *testing.T, ingressObj *v1.Ingress, expectedIngress pega
 
 // VerifyPegaIngress - Performs Pega Ingress assertions with the values as provided in default values.yaml
 func VerifyK8SIngress(t *testing.T, ingressObj *v1.Ingress, expectedIngress pegaIngress) {
-	require.Equal(t, "traefik", ingressObj.Annotations["kubernetes.io/ingress.class"])
+	require.Equal(t, "traefik", ingressObj.Spec.ingressClassName)
 	require.Equal(t, expectedIngress.Name, ingressObj.Spec.Rules[0].HTTP.Paths[0].Backend.Service.Name)
 	require.Equal(t, expectedIngress.Port, ingressObj.Spec.Rules[0].HTTP.Paths[0].Backend.Service.Port.Number)
 }
